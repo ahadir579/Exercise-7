673276450c6aafa4b915876d23bf20245df46a31
ahadir579 <139805536+ahadir579@users.noreply.github.com>
2023-07-29 22:16:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results
+// Addition 
+const additionResult = 7 + 1;
+console.log("Addition Result: " + additionResult);
+// Subtraction
+const subtractionResult = 9 - 1;
+console.log("Subtraction Result: " + subtractionResult);
+// Multiplication 
+const multiplicationResult = 1 * 8;
+console.log("Multiplication Result: " + multiplicationResult);
+// Division 
+const divisionResult = 8 / 1;
+console.log("Division Result: " + divisionResult);
